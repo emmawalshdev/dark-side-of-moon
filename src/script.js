44efa13b71940915cdf58f7734ea2fa3a66f9e8a
Emma Walsh <emmaharte33@outlook.com>
2023-03-31 21:55:18 +0100

    Add dampening to controls

--- src/script.js
+++ src/script.js
@@ -38,11 +38,11 @@ const parameters = {
 // controls
 const controls = new OrbitControls(camera, canvas);
 controls.maxPolarAngle = Math.PI
-controls.update();
-console.enableDamping = true;
+
+controls.enableDamping = true;
 controls.maxDistance = 500;
 controls.minDistance = 160;
-
+controls.update();
 
 // axes helper
 const axesHelper = new THREE.AxesHelper(500);
@@ -67,10 +67,7 @@ window.addEventListener('resize', () => {
 	renderer.setSize(sizes.width, sizes.height);
 	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 	
-})
-
-// object
-//const geometry = new THREE.BoxGeometry(2,2,2);
+});
 
 // texture loader
 const loadingManager = new THREE.LoadingManager();
@@ -89,7 +86,7 @@ moonTexture.minFilter = THREE.NearestFilter
 moonTexture.magFilter = THREE.NearestFilter
 moonTexture.generateMipmaps = false
 
-
+// stars
 for (let i=0; i<100; i++) {
 	const starGeometry = new THREE.TetrahedronGeometry(1, 0);
 	const star = new THREE.Mesh(starGeometry, starMaterial);
@@ -99,13 +96,10 @@ for (let i=0; i<100; i++) {
 	star.position.z = (Math.random() - 0.5) * 500
 }
 
-//sphere
+// moon
 const sphereGeometry = new THREE.SphereGeometry(100,50,100)
-const moon = new THREE.Mesh(sphereGeometry, material);
-//material.wireframe = true
-//scene.add(moon)
 
-// minimoon using phong material
+//  phong material
 const miniMoonMaterial = new THREE.MeshPhongMaterial({
 	map: moonTexture,
 	bumpMap: moonDispTexture,
@@ -115,22 +109,20 @@ const miniMoonMaterial = new THREE.MeshPhongMaterial({
 })
 
 const miniMoon = new THREE.Mesh(sphereGeometry, miniMoonMaterial);
-//material.wireframe = true
 scene.add(miniMoon)
-
-// miniMoon.position.z = 200
 
 const clock = new THREE.Clock();
 
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
+// light
 const light = new THREE.DirectionalLight(0xFFFFFF, 1);
 light.position.set(-100, 100, 900);
 scene.add(light);
 
 const ambLight = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( ambLight );
+scene.add(ambLight);
 
 
 gui
@@ -170,10 +162,11 @@ gui.addColor(parameters, 'color')
 function animate(){
 	
 	const elapsedTime = clock.getElapsedTime();
-	// miniMoon.rotation.y = -(0.1 * elapsedTime)
+	miniMoon.rotation.y = -(0.1 * elapsedTime)
 
-	// controls.autoRotate = true;
-	// controls.autoRotateSpeed = 0.6;
+	controls.autoRotate = true;
+	controls.autoRotateSpeed = 0.6;
+	
 	controls.update();
 
 	window.requestAnimationFrame(animate)
@@ -181,135 +174,4 @@ function animate(){
 	renderer.render(scene, camera);
 }
 
-animate()
-
-/**
- * Base
- */
-// Debug
-//const gui = new dat.GUI()
-
-// /**
-//  * Textures
-//  */
-// const textureLoader = new THREE.TextureLoader()
-// const matcapTexture = textureLoader.load('textures/matcaps/8.png')
-
-// /**
-//  * Fonts
-//  */
-// const fontLoader = new FontLoader()
-
-// fontLoader.load(
-//     '/fonts/helvetiker_regular.typeface.json',
-//     (font) =>
-//     {
-//         // Material
-//         const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
-
-//         // Text
-//         const textGeometry = new TextGeometry(
-//             'Emma Walsh',
-//             {
-//                 font: font,
-//                 size: 0.5,
-//                 height: 0.2,
-//                 curveSegments: 12,
-//                 bevelEnabled: true,
-//                 bevelThickness: 0.03,
-//                 bevelSize: 0.02,
-//                 bevelOffset: 0,
-//                 bevelSegments: 5
-//             }
-//         )
-//         textGeometry.center()
-
-//         const text = new THREE.Mesh(textGeometry, material)
-//         scene.add(text)
-
-//         // Donuts
-//         const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
-
-//         for(let i = 0; i < 100; i++)
-//         {
-//             const donut = new THREE.Mesh(donutGeometry, material)
-//             donut.position.x = (Math.random() - 0.5) * 10
-//             donut.position.y = (Math.random() - 0.5) * 10
-//             donut.position.z = (Math.random() - 0.5) * 10
-//             donut.rotation.x = Math.random() * Math.PI
-//             donut.rotation.y = Math.random() * Math.PI
-//             const scale = Math.random()
-//             donut.scale.set(scale, scale, scale)
-
-//             scene.add(donut)
-
-//         }
-//     }
-// )
-
-// /**
-//  * Sizes
-//  */
-// const sizes = {
-//     width: window.innerWidth,
-//     height: window.innerHeight
-// }
-
-
-
-// window.addEventListener('resize', () =>
-// {
-//     // Update sizes
-//     sizes.width = window.innerWidth
-//     sizes.height = window.innerHeight
-
-//     // Update camera
-//     camera.aspect = sizes.width / sizes.height
-//     camera.updateProjectionMatrix()
-
-//     // Update renderer
-//     renderer.setSize(sizes.width, sizes.height)
-//     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-// })
-
-// /**
-//  * Camera
-//  */
-// // Base camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-// camera.position.x = 1
-// camera.position.y = 1
-// camera.position.z = 2
-// scene.add(camera)
-
-
-
-// /**
-//  * Renderer
-//  */
-// const renderer = new THREE.WebGLRenderer({
-//     canvas: canvas
-// })
-// renderer.setSize(sizes.width, sizes.height)
-// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-// /**
-//  * Animate
-//  */
-// const clock = new THREE.Clock()
-
-// const tick = () =>
-// {
-//     const elapsedTime = clock.getElapsedTime()
-
-//     // Update controls
-//     controls.update()
-
-//     // Render
-//     renderer.render(scene, camera)
-
-//     // Call tick again on the next frame
-//     window.requestAnimationFrame(tick)
-// }
-
-// tick()
+animate();
